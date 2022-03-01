bb185418a884daf5724201e82124a1de12012c79
Jessica Colares <95100614+NamelessQueen@users.noreply.github.com>
2022-02-28 20:12:28 -0400

    Update Hello World.hs

--- Resoluções/Hello World.hs
+++ Resoluções/Hello World.hs
@@ -1,5 +1,21 @@
 -- Resolução da questão Hello World de Haskell no Exercism
 
+-- Instruções
+-- Aula introdutória de exercício clássico. Apenas diga "Hello, World!".
+
+-- "Hello, World!" é o primeiro programa tradicional para inciantes na programação numa nova linguagem ou ambiente.
+
+-- Os objetivos são simples:
+
+-- Escreva uma função que retorne uma string "Hello, World!".
+-- Rode um teste e observe se funciona.
+-- Envie sua solução e confirme no site.
+-- Se tudo der certo, você concluirá seu primeiro exercício.
+
+-- Para concluir esse exercício, você precisa implementar a função hello.
+
+-- Você encontrará a assinatura hello em seu lugar, mas você precisa definir a função.
+
 module HelloWorld (hello) where
 hello :: String
 hello = "Hello, World!"
